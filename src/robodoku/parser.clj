18f2262155fae635116ddfa11c188080e69995dd
worace <horace.d.williams@gmail.com>
2015-03-23 11:24:20 -0600

    move parsing code into parser namespace

---
+++ src/robodoku/parser.clj
@@ -0,0 +1,39 @@
+(ns robodoku.parser)
+
+(defn read-puzzle
+  "Returns seq of strings representing puzzle cell values (81 for 9x9 grid)"
+  [filepath]
+  (rest (clojure.string/split
+      (clojure.string/replace
+        (slurp (.getFile (clojure.java.io/resource filepath))) #"\n" "")
+      #"")))
+
+(defn puzzle-file
+  ([puzzle resource-dir] (clojure.string/join "/" [resource-dir puzzle]))
+  ([puzzle] (puzzle-file puzzle "puzzles")))
+
+(defn cell-possibilities
+  ([size] (clojure.string/join "" (range 1 (+ 1 size))))
+  ([] (cell-possibilities 9)))
+
+(defn grid-letters
+  ([size] (map str (map char (range 65 (+ 65 size)))))
+  ([] (grid-letters 9)))
+
+(defn cell-labels
+  ([size] (for [col (grid-letters size) row (range 1 (+ 1 size))]
+             (clojure.string/join "" [col row])))
+  ([] (cell-labels 9)))
+
+(defn fill-cells-with-possibilities
+  [cell-values grid-size]
+  (map (fn [cell-value]
+         (if (= " " cell-value)
+           (cell-possibilities grid-size)
+           cell-value)) cell-values))
+
+(defn parse-puzzle
+  [filename]
+  (let [cell-values (read-puzzle (puzzle-file filename))
+        size (int (Math/sqrt (count cell-values)))]
+    (zipmap (cell-labels size) (fill-cells-with-possibilities cell-values size))))
--- test/robodoku/core_test.clj
+++ test/robodoku/core_test.clj
@@ -2,36 +2,4 @@
   (:require [clojure.test :refer :all]
             [robodoku.core :refer :all]))
 
-(deftest constructing-puzzle-file-paths
-  (testing "it finds resource path given source dir and puzzle file"
-    (is (= "puzzles/two_by_two.txt" (puzzle-file "two_by_two.txt" "puzzles"))))
-  (testing "it defaults dir to puzzles"
-    (is (= "puzzles/two_by_two.txt" (puzzle-file "two_by_two.txt")))))
 
-(deftest test-read-puzzle
-  (testing "it returns seq of chars for puzzle"
-    (is (= ["1" "2" "2" "1"] (read-puzzle (puzzle-file "two_by_two.txt"))))))
-
-(deftest test-finding-cell-possibilities
-  (testing "it generates string of possibilities for grid of requested size"
-    (is (= "1234" (cell-possibilities 4))))
-  (testing "it defaults to 9"
-    (is (= "123456789" (cell-possibilities)))))
-
-(deftest test-finding-grid-letters
-  (testing "it finds appropriate number of grid letters"
-    (is (= ["A" "B" "C" "D"] (grid-letters 4))))
-  (testing "defaults to 9"
-    (is (= ["A" "B" "C" "D" "E" "F" "G" "H" "I"] (grid-letters)))))
-
-(deftest test-finding-cell-labels
-  (testing "it crosses rows and cols to generate cell labels"
-    (is (= ["A1" "A2" "B1" "B2"] (cell-labels 2))))
-  (testing "defaults to 9"
-    (is (= 81 (count (cell-labels))))))
-
-(deftest test-parsing-a-puzzle-file
-  (testing "it returns map of char label to char value"
-    (is (= {"A1" "1" "A2" "2" "B1" "2" "B2" "1"} (parse-puzzle "two_by_two.txt"))))
-  (testing "a blank square retains all possible values for that puzzle"
-    (is (= {"A1" "12" "A2" "2" "B1" "2" "B2" "1"} (parse-puzzle "two_by_two_with_blank.txt")))))
